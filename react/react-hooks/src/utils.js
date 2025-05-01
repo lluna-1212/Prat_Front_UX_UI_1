0819e6f3aaf645d26211b0fdf6781fe8629d6959
César P. <cesarlpb@gmail.com>
2025-05-01 12:13:08 +0200

    TODO: completar función de logger lado cliente

--- react/react-hooks/src/utils.js
+++ react/react-hooks/src/utils.js
@@ -1,10 +1,13 @@
 export default function log(string, type, isActive){
   // switch
 
-    // console.log()
-    
-    // console.warn()
+  // console.log()
 
-    // console.error
+  // console.warn()
 
+  // console.error
+
+  // TODO: completar fn de logger lado cliente de ejemplo
+
+  return { string, type, isActive };
 }
