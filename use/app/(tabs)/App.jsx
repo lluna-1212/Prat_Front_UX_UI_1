09b33cebbca6fe4bd32cd44355aeed5970201777
lluna-1212 <llunamelladofernandez12@gmail.com>
2025-05-07 16:57:39 +0200

    Cambios Use Lluna Mellado Fernández

---
+++ use/app/(tabs)/App.jsx
@@ -0,0 +1,44 @@
+import { createContext, use } from 'react';
+
+const ThemeContext = createContext(null);
+
+export default function MyApp() {
+  return (
+    <ThemeContext.Provider value="dark">
+      <Form />
+    </ThemeContext.Provider>
+  )
+}
+
+function Form() {
+  return (
+    <Panel title="Bienvenido">
+      <Button show={true}>Registrarse</Button>
+      <Button show={false}>Iniciar sesión</Button>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = use(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
+
+function Button({ show, children }) {
+  if (show) {
+    const theme = use(ThemeContext);
+    const className = 'button-' + theme;
+    return (
+      <button className={className}>
+        {children}
+      </button>
+    );
+  }
+  return false
+}
