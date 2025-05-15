519b000403003d4b4ed62ee919e196ff81485c47
César P. <cesarlpb@gmail.com>
2025-05-15 17:57:45 +0200

    añado estilos y componente con memo para comparar

--- react/apuntes/ejemplos/src/App.jsx
+++ react/apuntes/ejemplos/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Greeting from './Greeting'
+import GreetingMemo from './GreetingMemo'
 
 function App() {
   
   return (
     <>
+      <h1>I'ts <code>memo</code> time!</h1>
+      <h2>(real no fake)</h2>
       <Greeting />
+      <GreetingMemo />
     </>
   )
 }
