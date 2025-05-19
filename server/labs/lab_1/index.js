3aaba4f96875a2efadf043435d999f4fecaa1189
César P <cesarlpb@gmail.com>
2025-05-19 19:24:44 +0200

    completamos el CRUD con PUT y DELETE

--- server/labs/lab_1/index.js
+++ server/labs/lab_1/index.js
@@ -36,17 +36,42 @@ app.post('/tareas', (req, res) => {
 })
 app.put('/tareas/:id', (req, res) => {
   // Encontrar la tarea por id
-  
+  const tareaId = req.params.id; 
+  const tarea = req.body;
+  console.log("Tarea recibida:", tarea);
+  const tareaIndex = tareas.findIndex(
+    tarea => `${tarea.id}` === `${tareaId}`
+  );
+  if(tareaIndex === -1) {
+    res.status(404).json({ error: "Tarea no encontrada" });
+    return;
+  }
+  // tareas[tareaIndex] = tarea; // ❌ sustituye por completo el objeto
   // Editar el campo que me viene en el body
-
+  if (tarea.titulo) {
+    tareas[tareaIndex].titulo = tarea.titulo;
+  }
+  if (tarea.completada) {
+    tareas[tareaIndex].completada = tarea.completada;
+  }
   // Guardar la tarea en el array (en el id correcto)
-
+  console.log("Tarea editada:");
+  console.log(tareas[tareaIndex]); 
+  res.status(200).json(tareas[tareaIndex]);
 })
 app.delete('/tareas/:id', (req, res) => {
   // Encontrar la tarea por id
+  const tareaId = req.params.id;
+  const tareaIndex = tareas.findIndex(
+    tarea => `${tarea.id}` === `${tareaId}`
+  );
+
+  // Podríamos poner un bucle if para verificar si existe la tarea
+  // Si el id no es mayor que cero o no es entero => 400 
 
   // Eliminar la tarea
-  
+  tareas.pop(tareaIndex);
+  res.status(204).json();
 })
 // Iniciar el servidor
 app.listen(PORT, () => {
